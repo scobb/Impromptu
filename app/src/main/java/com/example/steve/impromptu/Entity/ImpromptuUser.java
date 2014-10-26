a202a7090bd1570d8583b2c5b89e00dc3c7d38e6
Steve Cobb <scobb@alumni.lsu.edu>
2014-10-26 16:01:29 -0500

    Adding facebook SDK. Changed name of User to ImpromptuUser

---
+++ app/src/main/java/com/example/steve/impromptu/Entity/ImpromptuUser.java
@@ -0,0 +1,30 @@
+package com.example.steve.impromptu.Entity;
+
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.steve.impromptu.Login.ActivityLogin;
+import com.example.steve.impromptu.Main.ActivityMain;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+/**
+ * ImpromptuUser object
+ * Created by scobb on 10/19/14.
+ */
+public class ImpromptuUser extends ParseUser{
+    public ImpromptuUser(String username, String pw, String email) {
+        // use parse's interface to set basic info
+        super();
+        this.setUsername(username);
+        this.setPassword(pw);
+        this.setEmail(email);
+    }
+
+    public void RegisterUser() {
+
+    }
+
+}
