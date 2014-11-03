502e0fd22f2d98a2897bc5f52f8d9a23deb6dbbd
Stephen Arifin <stephen.arifin@gmail.com>
2014-11-02 21:06:22 -0600

    working stream

--- app/src/main/java/com/example/steve/impromptu/Entity/StreamPost.java
+++ app/src/main/java/com/example/steve/impromptu/Entity/StreamPost.java
@@ -1,5 +1,7 @@
 package com.example.steve.impromptu.Entity;
 
+import com.example.steve.impromptu.R;
+
 import java.util.Date;
 import java.util.HashMap;
 
@@ -21,7 +23,7 @@ public class StreamPost {
     public HashMap<String, String> getHashMap(){
         HashMap<String, String> map = new HashMap<String, String>();
         map.put("user", user.getName());
-//        map.put("picture", Integer.toString(user.getPicture()));
+        map.put("picture", Integer.toString(R.drawable.ic_launcher));
         map.put("date", date.toString());
         map.put("content", content);
         return map;
