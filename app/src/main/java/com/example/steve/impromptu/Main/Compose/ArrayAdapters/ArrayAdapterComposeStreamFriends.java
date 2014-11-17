116844cf267087039e800db8f8997370d4a9369c
SunburtReynolds <jon.reynolds456@utexas.edu>
2014-11-16 18:21:37 -0600

    compose stream fully functional

---
+++ app/src/main/java/com/example/steve/impromptu/Main/Compose/ArrayAdapters/ArrayAdapterComposeStreamFriends.java
@@ -0,0 +1,105 @@
+package com.example.steve.impromptu.Main.Compose.ArrayAdapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.example.steve.impromptu.Entity.Group;
+import com.example.steve.impromptu.Entity.ImpromptuUser;
+import com.example.steve.impromptu.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jonreynolds on 10/31/14.
+ */
+public class ArrayAdapterComposeStreamFriends extends ArrayAdapter<ImpromptuUser> {
+
+    private ArrayList<ImpromptuUser> friendList;
+    private ArrayList<Group> groupsList;
+    private Context context;
+
+    public ArrayAdapterComposeStreamFriends(Context context, int textViewResourceId,
+                                          ArrayList<ImpromptuUser> friendList, ArrayList<Group> groupsList) {
+        super(context, textViewResourceId, friendList);
+        this.context = context;
+        this.friendList = new ArrayList<ImpromptuUser>();
+        this.groupsList = new ArrayList<Group>();
+        this.friendList.addAll(friendList);
+        this.groupsList.addAll(groupsList);
+    }
+
+    static class ViewHolder {
+        protected TextView text;
+        protected CheckBox checkbox;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+//        ViewHolder holder = null;
+        Log.v("ConvertView", String.valueOf(position));
+
+        View view = null;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.template_friend_item, null);
+            final ViewHolder viewHolder = new ViewHolder();
+            viewHolder.text = (TextView) view.findViewById(R.id.templateFriendItem_textView_friendName);
+            viewHolder.checkbox = (CheckBox) view.findViewById(R.id.templateFriendItem_checkBox_check);
+            viewHolder.checkbox
+                    .setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView,
+                                                     boolean isChecked) {
+                            ImpromptuUser friend = (ImpromptuUser) viewHolder.checkbox
+                                    .getTag();
+                            if (isChecked) {
+                                friend.setSelected(true);
+                            }
+
+                            else {
+                                friend.setSelected(false);
+
+                                for (Group group : groupsList) {
+                                    ArrayList<ImpromptuUser> friends = (ArrayList<ImpromptuUser>) group.getFriendsInGroup();
+                                    if (listContainsFriend(friends, friend)) {
+                                        group.setSelected(false);
+                                    }
+                                }
+                            }
+                        }
+                    });
+            view.setTag(viewHolder);
+            viewHolder.checkbox.setTag(friendList.get(position));
+        } else {
+            view = convertView;
+            ((ViewHolder) view.getTag()).checkbox.setTag(friendList.get(position));
+        }
+        ViewHolder holder = (ViewHolder) view.getTag();
+        holder.text.setText(friendList.get(position).getName());
+        holder.checkbox.setChecked(friendList.get(position).isSelected());
+        return view;
+    }
+
+    public Boolean listContainsFriend(ArrayList<ImpromptuUser> list, ImpromptuUser friend) {
+        Boolean contains = false;
+
+        for (ImpromptuUser frd : list) {
+            if (frd.getName().equals(friend.getName())) {
+                contains = true;
+                break;
+            }
+        }
+
+        return contains;
+    }
+}
---
+++ app/src/main/java/com/example/steve/impromptu/Main/Compose/FragmentComposeStreamGroups.java
@@ -0,0 +1,217 @@
+package com.example.steve.impromptu.Main.Compose;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.steve.impromptu.Entity.Event;
+import com.example.steve.impromptu.Entity.Group;
+import com.example.steve.impromptu.Entity.ImpromptuUser;
+import com.example.steve.impromptu.Main.ActivityMain;
+import com.example.steve.impromptu.Main.Compose.ArrayAdapters.ArrayAdapterComposeStreamGroups;
+import com.example.steve.impromptu.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+/**
+ * Created by jonreynolds on 11/9/14.
+ */
+public class FragmentComposeStreamGroups extends Fragment {
+
+
+    ListView vGroupsList;
+    LinearLayout vOkay;
+    LinearLayout vCancel;
+    ArrayList<Group> eventStreamGroupsList;
+    ArrayList<Group> userGroupsList;
+
+    ArrayAdapterComposeStreamGroups groupsAdapter = null;
+
+    OnComposeStreamChooseGroupsFinishedListener mCallback;
+
+    // Container Activity must implement this interface
+    public interface OnComposeStreamChooseGroupsFinishedListener {
+        public void onComposeStreamChooseGroupsFinished();
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View fragmentView = inflater.inflate(R.layout.fragment_compose_streamgroups, container, false);
+        vGroupsList = (ListView) fragmentView.findViewById(R.id.fragComposeStreamGroups_listView_groupsList);
+        vOkay = (LinearLayout) fragmentView.findViewById(R.id.fragComposeStreamGroups_linearLayout_okay);
+        vCancel = (LinearLayout) fragmentView.findViewById(R.id.fragComposeStreamGroups_linearLayout_cancel);
+
+        ImpromptuUser currentUser = (ImpromptuUser) ImpromptuUser.getCurrentUser();
+        userGroupsList = (ArrayList<Group>) currentUser.getGroups();
+
+        ActivityMain myActivity = (ActivityMain) getActivity();
+        Event myEvent = myActivity.getComposeEvent();
+
+        // initialize all groups to unselected
+        for (Group group : userGroupsList) {
+            group.setSelected(false);
+        }
+
+        eventStreamGroupsList = (ArrayList<Group>) myEvent.getStreamGroups();
+        if (!eventStreamGroupsList.isEmpty() && !userGroupsList.isEmpty()) {
+            // there are groups to stream to && user has groups
+
+            Iterator<Group> iterEventGroups = eventStreamGroupsList.iterator();
+            Iterator<Group> iterUserGroups = userGroupsList.iterator();
+
+            Group eventGroup;
+            Group userGroup;
+
+            while (iterEventGroups.hasNext() && iterUserGroups.hasNext()) {
+                // Note: eventStreamGroupsList is a subset of userGroupsList
+
+                eventGroup = iterEventGroups.next();
+                userGroup = iterUserGroups.next();
+
+                int comp = eventGroup.compareTo(userGroup);
+                if (comp == 0) {
+                    // they are the same
+
+                    userGroup.setSelected(true);
+
+                } else if (comp > 0) {
+                    // eventGroup is "greater" than userGroup
+
+                    while (((eventGroup.compareTo(userGroup)) != 0) && iterUserGroups.hasNext()) {
+                        // keep looking for an equivalent group
+                        userGroup = iterUserGroups.next();
+                    }
+                    userGroup.setSelected(true);
+
+                } else {
+                    // shouldn't get this case because both lists of groups are sorted alphabetically
+                    // and eventStreamGroupsList is a subset of userGroupsList
+                }
+            }
+        }
+
+        groupsAdapter = new ArrayAdapterComposeStreamGroups(getActivity(), R.layout.template_friend_item, userGroupsList);
+        vGroupsList.setAdapter(groupsAdapter);
+
+        vOkay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ActivityMain myActivity = (ActivityMain) getActivity();
+                Event myEvent = myActivity.getComposeEvent();
+                ArrayList<ImpromptuUser> eventStreamFriendsList = (ArrayList<ImpromptuUser>) myEvent.getStreamFriends();
+
+                for (Group group : userGroupsList) {
+
+                    Group grp = listContainsGroup(eventStreamGroupsList, group);
+
+                    if (grp != null) {
+                        // if this group was in the eventStreamGroupsList && is not selected, remove it
+                        if (!(group.isSelected())) {
+                            eventStreamGroupsList.remove(grp);
+                            // remove friends that belong to group from eventStreamFriendsList
+                            ArrayList<ImpromptuUser> friends = (ArrayList<ImpromptuUser>)group.getFriendsInGroup();
+                            for (ImpromptuUser friend : friends) {
+                                // TODO: potential hazard with remove
+                                eventStreamFriendsList.remove(friend);
+                            }
+                        }
+                    }
+
+                    if (group.isSelected()) {
+                        // if this is one of the selected groups && it was not already in the eventStreamGroupsList, add it
+                        if (grp == null)
+                            eventStreamGroupsList.add(group);
+                    }
+                }
+                Collections.sort(eventStreamGroupsList); // make sure eventStreamGroupsList is sorted alphabetically
+                myEvent.setStreamGroups(eventStreamGroupsList);
+
+                for (Group group : eventStreamGroupsList) {
+                    ArrayList<ImpromptuUser> friendsList = (ArrayList<ImpromptuUser>) group.getFriendsInGroup();
+
+                    for (ImpromptuUser friend : friendsList) {
+
+                        ImpromptuUser frd = listContainsFriend(eventStreamFriendsList, friend);
+
+                        if (frd == null) {
+                            eventStreamFriendsList.add(friend);
+                        }
+
+                    }
+                }
+                Collections.sort(eventStreamFriendsList);
+                myEvent.setStreamFriends(eventStreamFriendsList);
+
+                String test = "";
+                test = test + "Groups: ";
+                for (Group group : eventStreamGroupsList) {
+                    if (group.isSelected())
+                        test = test + group.getGroupName();
+                }
+
+                Toast.makeText(getActivity(), test, Toast.LENGTH_SHORT).show();
+
+
+                mCallback.onComposeStreamChooseGroupsFinished();
+            }
+        });
+
+        vCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mCallback.onComposeStreamChooseGroupsFinished();
+            }
+        });
+
+        return fragmentView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnComposeStreamChooseGroupsFinishedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnComposeStreamFinishedListener");
+        }
+    }
+
+    public ImpromptuUser listContainsFriend(ArrayList<ImpromptuUser> list, ImpromptuUser friend) {
+
+        for (ImpromptuUser frd : list) {
+            if (frd.getName().equals(friend.getName())) {
+                return frd;
+            }
+        }
+
+        return null;
+    }
+
+    public Group listContainsGroup(ArrayList<Group> list, Group group) {
+
+        for (Group grp : list) {
+            if (group.getGroupName().equals(grp.getGroupName())) {
+                return grp;
+            }
+        }
+
+        return null;
+    }
+
+}
