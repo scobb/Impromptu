fc19a17068399ce03f8a0a50cf32d923f4aa0d65
Steve Cobb <scobb1@utexas.edu>
2014-11-27 10:25:21 -0600

    Added destroyFriendship cloud function

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -5,36 +5,47 @@ Parse.Cloud.define("hello", function(request, response) {
 	response.success("Hello world!");
 });
 
-// Parse.Cloud.afterSave("Event", function(request) {
-// Parse.Cloud.useMasterKey();
-// var event = request.object;
-// var Event = Parse.Object.extend("Event");
-// console.log("Event is " + event);
-// console.log("eventId: " + event.id);
-// // users whose events array contain the event in question
-// var relation = event.relation("streamFriends");
-// var q = relation.query();
-//	
-// q.find().then(function(results) {
-// console.log("In q, results are : " + results);
-// for (var j = 0; j < results.length; j++) {
-// var userEvents = results[j].get("events")
-// var needToAdd = true;
-// for (var k = 0; k < userEvents.length; k++) {
-// if (userEvents[k].id == event.id) {
-// needToAdd = false;
-// break;
-// }
-// }
-// if (needToAdd)
-// userEvents.push(event);
-// }
-// Parse.Object.saveAll(results);
-//
-// }, function(error) {
-// console.log("Error: " + error.message);
-// });
-// });
+Parse.Cloud.define("destroyFriendship", function(request, response) {
+	Parse.Cloud.useMasterKey();
+	var fromId = request.params.fromId;
+	var toId = request.params.toId;
+	
+	// generate the two queries
+	var q1 = new Parse.Query(Parse.User);
+	var q2 = new Parse.Query(Parse.User);
+	
+	// execute both queries in parallel. When they complete...
+	Parse.Promise.when([q1.get(fromId), q2.get(toId)]).then(function(from, to) {
+		console.log("from: " + from);
+		console.log("to: " + to);
+		var fromFriends = from.get('friends');
+		var toFriends = to.get('friends');
+		var fromFriendIds = _.map(fromFriends, function(friend) {
+			return friend.id;
+		})
+		var toFriendIds = _.map(toFriends, function(friend) {
+			return friend.id;
+		})
+		if (fromFriendIds.indexOf(to.id) < 0) {
+			console.log(to.id + " is not a friend of " + from.id);
+		} else {
+			console.log(to.id + " is a friend of " + from.id + ". Removing.");
+			fromFriends.splice(fromFriendIds.indexOf(to.id), 1);
+		}
+		if (toFriendIds.indexOf(from.id) < 0) {
+			console.log(from.id + " is not a friend of " + to.id);
+		} else {
+			console.log(from.id + " is a friend of " + to.id + ". Removing.");
+			toFriends.splice(toFriendIds.indexOf(from.id), 1);
+		}
+		return Parse.Object.saveAll([from, to]);
+	}).then(function() {
+		response.success("Saved.");
+	}, function(error) {
+		response.error(error);
+	})
+	
+})
 
 Parse.Cloud.define("addNewEvent", function(request, response) {
 	Parse.Cloud.useMasterKey();
