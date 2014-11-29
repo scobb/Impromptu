60e6de6cc701dd555666143543717d9012f68297
Steve Cobb <scobb1@utexas.edu>
2014-11-28 21:55:48 -0600

    Added push for accepted friend request

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -253,101 +253,99 @@ Parse.Cloud.job("cleanAllEvents", function(request, response) {
 })
 
 Parse.Cloud.define("eventCleanup", function(request, response) {
-	Parse.Cloud.useMasterKey();
-	var Event = Parse.Object.extend("Event");
-	var query = new Parse.Query(Event);
-	var eventId = request.params.eventId;
-	query.get(eventId, {
-		success : function(event) {
-			var innerQuery = new Parse.Query(Parse.User);
-			// users whose events array contain the event in question
-			innerQuery.equalTo("events", event)
-			innerQuery.find({
-				success : function(users) {
-					for (var j = 0; j < users.length; j++) {
-						user = users[j];
-						var events = user.get("events");
-						for (var i = events.length - 1; i >= 0; i--) {
-							if (events[i].id == eventId) {
-								// remove the event from their array
-								console.log("Removing an object because " + events[i].id + " == " + eventId);
-								events.splice(i, 1);
-							}
-						}
-						console.log("events after removal: " + events);
-					}
-					// persist updated users
-					Parse.Object.saveAll(users, {
-						success : function(list) {
-							// destroy the event
-							event.destroy({
-								success : function() {
-									response.success("Yay.");
+       Parse.Cloud.useMasterKey();
+       var Event = Parse.Object.extend("Event");
+       var query = new Parse.Query(Event);
+       var eventId = request.params.eventId;
+       query.get(eventId, {
+               success : function(event) {
+                       var innerQuery = new Parse.Query(Parse.User);
+                       // users whose events array contain the event in question
+                       innerQuery.equalTo("events", event)
+                       innerQuery.find({
+                               success : function(users) {
+                                       for (var j = 0; j < users.length; j++) {
+                                               user = users[j];
+                                               var events = user.get("events");
+                                               for (var i = events.length - 1; i >= 0; i--) {
+                                                       if (events[i].id == eventId) {
+                                                               // remove the event from their array
+                                                               console.log("Removing an object because " + events[i].id + " == " + eventId);
+                                                               events.splice(i, 1);
+                                                       }
+                                               }
+                                               console.log("events after removal: " + events);
+                                       }
+                                       // persist updated users
+                                       Parse.Object.saveAll(users, {
+                                               success : function(list) {
+                                                       // destroy the event
+                                                       event.destroy({
+                                                               success : function() {
+                                                                       response.success("Yay.");
 
-								},
-								error : function(error) {
-									response.error(error);
-								}
-							})
+                                                               },
+                                                               error : function(error) {
+                                                                       response.error(error);
+                                                               }
+                                                       })
 
-						},
-						error : function(error) {
-							response.error(error);
+                                               },
+                                               error : function(error) {
+                                                       response.error(error);
 
-						},
+                                               },
 
-					});
+                                       });
 
-				},
-				error : function(error) {
-					reseponse.error(error)
-				}
-			})
-		},
-		error : function(object, error) {
-			console.log("Query unsuccessful.");
-			response.error(error);
-		}
-	});
+                               },
+                               error : function(error) {
+                                       reseponse.error(error)
+                               }
+                       })
+               },
+               error : function(object, error) {
+                       console.log("Query unsuccessful.");
+                       response.error(error);
+               }
+       });
 });
 
 Parse.Cloud.define("acceptFriendRequest", function(request, response) {
 	Parse.Cloud.useMasterKey();
 	var fromId = request.params.fromId;
 	var toId = request.params.toId;
-	var query = new Parse.Query(Parse.User);
-	query.get(fromId, {
-		success : function(from) {
-			console.log("First query successful.");
-			var innerQuery = new Parse.Query(Parse.User);
-			innerQuery.get(toId, {
-				success : function(to) {
-					console.log("Second query successful.");
-					from.get('friends').push(to);
-					to.get('friends').push(from);
-					Parse.Object.saveAll([ to, from ], {
-						success : function(list) {
-							response.success("Yay.");
-
-						},
-						error : function(error) {
-							response.error(error);
-
-						},
-
-					});
-
-				},
-				error : function(error) {
-					response.error(error);
-				}
-			});
-
-		},
-		error : function(error) {
-			console.log("First query error." + error);
-			response.error(error);
+	var q = new Parse.Query(Parse.User);
+	var q2 = new Parse.Query(Parse.User);
+	
+	Parse.Promise.when([q.get(fromId), q2.get(toId)]).then(function(from, to) {
+		var q3 = new Parse.Query('Installation');
+		q3.equalTo('user', from);
+		var promise = Parse.Push.send({
+			where: q3,
+			data: {
+				alert: to.get("name") + ' accepted your friend request.'
+			}
+		});
+		console.log("Queries successful..");
+		var fromIds = _.map(from.get('friends'), function(friend) {
+			return friend.id;
+		})
+		var toIds = _.map(to.get('friends'), function(friend) {
+			return friend.id;
+		})
+		if (fromIds.indexOf(to.id) < 0) {
+			from.get('friends').push(to);
 		}
-	});
+		if (toIds.indexOf(from.id) < 0) {
+			to.get('friends').push(from);
+		}
+		return Parse.Promise.when([promise, Parse.Object.saveAll([ to, from ])]);
+	}, function(error) {
+		console.log("Error: " + error);
+		response.error(error);
+	}).then(function() {
+		response.success('boom');
+	})
 
 });
