5c4cb27be5eb6843524614e569d7cfea1d832a8c
Stephen Arifin <stephen.arifin@gmail.com>
2014-12-01 10:24:07 -0600

    people attending basic functionality

---
+++ app/src/main/java/com/example/steve/impromptu/Main/Compose/ArrayAdapters/ArrayAdapterPeopleAttending.java
@@ -0,0 +1,81 @@
+package com.example.steve.impromptu.Main.Compose.ArrayAdapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.steve.impromptu.Entity.Event;
+import com.example.steve.impromptu.Entity.ImpromptuUser;
+import com.example.steve.impromptu.Main.Compose.FragmentComposeType;
+import com.example.steve.impromptu.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by stephen on 12/1/14.
+ */
+
+
+public class ArrayAdapterPeopleAttending extends ArrayAdapter<ImpromptuUser> {
+
+    private ArrayList<ImpromptuUser> userList;
+    private Context context;
+
+    private LayoutInflater l_Inflater;
+
+    public ArrayAdapterPeopleAttending(Context context, int layoutResourceId,
+                                       List<ImpromptuUser> users) {
+
+        super(context, layoutResourceId, users);
+        this.context = context;
+        this.userList = new ArrayList<ImpromptuUser>();
+        this.userList.addAll(users);
+        l_Inflater = LayoutInflater.from(context);
+
+    }
+
+    @Override
+    public int getCount() {
+        return userList.size();
+    }
+
+    @Override
+    public ImpromptuUser getItem(int position) {
+        return userList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.v("ConvertView", String.valueOf(position));
+
+        View view = null;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.template_friend_attending_item, null);
+
+            ImageView vImage = (ImageView) view.findViewById(R.id.templateFriendAttendingItem_imageView_profilePic);
+            TextView vText = (TextView) view.findViewById(R.id.templateFriendAttendingItem_textView_name);
+
+            vImage.setImageBitmap(getItem(position).getPicture());
+            vText.setText(userList.get(position).getName());
+        }
+        else {
+            view = convertView;
+        }
+        return view;
+    }
+
+}
